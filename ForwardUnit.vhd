2131004d69472d9cd0bd9c3ec391bf96e990d9be
RamySaied1 <ramy.m.saied@gmail.com>
2019-04-22 22:15:21 +0200

    Add Memory selection Unit without testing

--- ForwardUnit.vhd
+++ ForwardUnit.vhd
@@ -27,8 +27,8 @@ ARCHITECTURE ForwardUnitArch of ForwardUnit is
     signal MemWbWb1,MemWbWb2 : STD_LOGIC;       
     
     ----------     from above signal we determine the output value between the next values 
-    Signal RSrc1Value,RDst1Value,RSrc2Value,RDst2Value:  STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0)  ------- the originals value if there is no forwarding 
-    Signal AluResult1,AluResult2,WbValue1,WbValue2:  STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0)  ------- the forwarding from Ex|mem buffer 
+    Signal RSrc1Value,RDst1Value,RSrc2Value,RDst2Value:  STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);  ------- the originals value if there is no forwarding 
+    Signal AluResult1,AluResult2:  STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);  ------- the forwarding from Ex|mem buffer 
     Signal WbValue1,WbValue2:  STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);    ----- the forwarding from mem/Wb buffer
 
     -----------------------------------------------------
@@ -46,16 +46,17 @@ BEGIN
     ----------------------------------------------------------
     process (IdEXRsc1,ExMemRdst1,ExMemRdst2,ExMemWb1,ExMemWb2,MemWbRdst1,MemWbRdst2,MemWbWb1,MemWbWb2)
         begin 
-            if (ExMemWb2 and (ExMemRdst2 =IdEXRsc1) ) then 
+            if ((ExMemWb2='1') and (ExMemRdst2 =IdEXRsc1) ) then 
                 Src1 <= AluResult2;
-            elsif (ExMemWb1 and (ExMemRdst1 =IdEXRsc1) ) then 
+            elsif ((ExMemWb1='1') and (ExMemRdst1 =IdEXRsc1) ) then 
                 Src1 <= AluResult1;
-            elsif (MemWbWb2 and (MemWbRdst2 =IdEXRsc1) ) then 
+            elsif (MemWbWb2='1' and (MemWbRdst2 =IdEXRsc1) ) then 
                 Src1 <= WbValue2;
-            elsif (MemWbWb1 and (MemWbRdst1 =IdEXRsc1) ) then 
+            elsif (MemWbWb1='1' and (MemWbRdst1 =IdEXRsc1) ) then 
                 Src1 <= WbValue1;
             else 
                 Src1 <= RSrc1Value;
+            end if;
             
         end process;
     ----------------------------------------------------------------------
@@ -63,16 +64,18 @@ BEGIN
     ----------------------------------------------------------
     process (IdEXRdst1,ExMemRdst1,ExMemRdst2,ExMemWb1,ExMemWb2,MemWbRdst1,MemWbRdst2,MemWbWb1,MemWbWb2)
         begin 
-            if (ExMemWb2 and (ExMemRdst2 =IdEXRdst1) ) then 
+            if (ExMemWb2='1' and (ExMemRdst2 =IdEXRdst1) ) then 
                 Dst1 <= AluResult2;
-            elsif (ExMemWb1 and (ExMemRdst1 =IdEXRdst1) ) then 
+            elsif (ExMemWb1='1' and (ExMemRdst1 =IdEXRdst1) ) then 
                 Dst1 <= AluResult1;
-            elsif (MemWbWb2 and (MemWbRdst2 =IdEXRdst1) ) then 
+            elsif (MemWbWb2='1' and (MemWbRdst2 =IdEXRdst1) ) then 
                 Dst1 <= WbValue2;
-            elsif (MemWbWb1 and (MemWbRdst1 =IdEXRdst1) ) then 
+            elsif (MemWbWb1='1'and (MemWbRdst1 =IdEXRdst1) ) then 
                 Dst1 <= WbValue1;
             else 
                 Dst1 <= RDst1Value;
+            end if;
+
             
         end process;
     ----------------------------------------------------------------------
@@ -81,16 +84,18 @@ BEGIN
   ----------------------------------------------------------
     process (IdEXRsc2,ExMemRdst1,ExMemRdst2,ExMemWb1,ExMemWb2,MemWbRdst1,MemWbRdst2,MemWbWb1,MemWbWb2)
         begin 
-            if (ExMemWb2 and (ExMemRdst2 =IdEXRsc2) ) then 
+            if (ExMemWb2='1' and (ExMemRdst2 =IdEXRsc2) ) then 
                 Src2 <= AluResult2;
-            elsif (ExMemWb1 and (ExMemRdst1 =IdEXRsc2) ) then 
+            elsif (ExMemWb1='1' and (ExMemRdst1 =IdEXRsc2) ) then 
                 Src2 <= AluResult1;
-            elsif (MemWbWb2 and (MemWbRdst2 =IdEXRsc2) ) then 
+            elsif (MemWbWb2='1' and (MemWbRdst2 =IdEXRsc2) ) then 
                 Src2 <= WbValue2;
-            elsif (MemWbWb1 and (MemWbRdst1 =IdEXRsc2) ) then 
+            elsif (MemWbWb1='1' and (MemWbRdst1 =IdEXRsc2) ) then 
                 Src2 <= WbValue1;
             else 
                 Src2 <= RSrc2Value;
+            end if;
+
             
         end process;
     ----------------------------------------------------------------------
@@ -99,16 +104,18 @@ BEGIN
     ----------------------------------------------------------
     process (IdEXRdst2,ExMemRdst1,ExMemRdst2,ExMemWb1,ExMemWb2,MemWbRdst1,MemWbRdst2,MemWbWb1,MemWbWb2)
         begin 
-            if (ExMemWb2 and (ExMemRdst2 =IdEXRdst2) ) then 
+            if (ExMemWb2='1' and (ExMemRdst2 =IdEXRdst2) ) then 
                 Dst2 <= AluResult2;
-            elsif (ExMemWb1 and (ExMemRdst1 =IdEXRdst2) ) then 
+            elsif (ExMemWb1='1' and (ExMemRdst1 =IdEXRdst2) ) then 
                 Dst2 <= AluResult1;
-            elsif (MemWbWb2 and (MemWbRdst2 =IdEXRdst2) ) then 
+            elsif (MemWbWb2='1' and (MemWbRdst2 =IdEXRdst2) ) then 
                 Dst2 <= WbValue2;
-            elsif (MemWbWb1 and (MemWbRdst1 =IdEXRdst2) ) then 
+            elsif (MemWbWb1='1' and (MemWbRdst1 =IdEXRdst2) ) then 
                 Dst2 <= WbValue1;
             else 
                 Dst2 <= RDst2Value;  
+             end if;
+
         end process;
     ---------------------------------------------------------------------
     
--- MemorySelectionUnit.vhd
+++ MemorySelectionUnit.vhd
@@ -5,9 +5,11 @@ use work.constants.all;
 -- MemorySelectionUnit Entity
 
 ENTITY MemorySelectionUnit IS
+      Generic(wordSize:integer :=16);
 	PORT(
             IDEXBuffer: in STD_LOGIC_VECTOR(IDEXLength DOWNTO 0);
-            EXMEMBuffer: out STD_LOGIC_VECTOR(EXMEMLength DOWNTO 0)
+            EXMEMBuffer: out STD_LOGIC_VECTOR(EXMEMLength DOWNTO 0);
+            Rsrc1,Rdst1,Rsrc2,Rdst2 : STD_LOGIC_VECTOR(wordSize-1 downto 0)  -- we must use the values after forwarding 
 		);
 
 END ENTITY MemorySelectionUnit;
@@ -16,9 +18,36 @@ END ENTITY MemorySelectionUnit;
 -- MemorySelectionUnitister Architecture
 
 ARCHITECTURE MemorySelectionUnitArch of MemorySelectionUnit is
+SIGNAL  mw1,mw2,mr1,mr2,skop1,skop2 : STD_LOGIC; ---     form IDEXbuffer ;
 
+SIGNAL  mw,mr,skop : STD_LOGIC;      -----   value the will be saved in EXMEMBuffer;
+SIGNAL RsrcMemory,RdstMemory : STD_LOGIC_VECTOR (wordSize-1 downto 0);
+
+------------- important---------- assume that no two instruction will access memry in the same batch
+----------------- this circuit output depent on decoding output
 BEGIN
 
-
+      process (mw1,mw2,mr1,mr2,skop1,skop2)
+            begin 
+                  if (skop1='1' or mw1='1' or mr1='1') then 
+                        skop<= skop1;
+                        mr <= mr1;
+                        mw <= mw1;
+                        RsrcMemory <= Rsrc1;
+                        RdstMemory <= Rdst1;
+                  elsif (skop2='1' or mw2='1' or mr2='1') then 
+                        skop<= skop2;
+                        mr <= mr2;
+                        mw <= mw2;
+                        RsrcMemory <= Rsrc2;
+                        RdstMemory <= Rdst2;
+                  else 
+                       skop <= '0';
+                       mr <= '0';
+                       mw <= '0';
+                       RsrcMemory <= (others => '0');   ----- any value not importnat 
+                       RdstMemory <= (others => '0') ;  ----- any value not importnat 
+                  end if;
+      end process;
 
 END ARCHITECTURE;
