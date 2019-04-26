cf4010b96bb987bcdcba17e0c1f73b7b91bccaf0
RamySaied1 <ramy.m.saied@gmail.com>
2019-04-26 19:06:09 +0200

    add pc control logic

--- PCControlUnit.vhd
+++ PCControlUnit.vhd
@@ -7,7 +7,9 @@ use work.constants.all;
 ENTITY PCControlUnit IS
     Generic(PCSize: integer :=32);
 	PORT(
-        clk: in STD_LOGIC;
+        clk,reset,ExPcEnable,MemPcEnable: in STD_LOGIC;
+        ControlPCoperation: in STD_LOGIC_VECTOR(1 downto 0);
+        ExPC,MemPc: in STD_LOGIC_Vector(PCSize-1 DownTO 0); 
         PCReg: out STD_LOGIC_VECTOR(PCSize-1 DOWNTO 0)
     );
 
@@ -17,8 +19,27 @@ END ENTITY PCControlUnit;
 -- PCControlUnit Architecture
 
 ARCHITECTURE PCControlUnitArch of PCControlUnit is
-
+    Signal PCcurrent,tempF ,tempA: STD_LOGIC_VECTOR(PCSize-1 DOWNTO 0);
+    Signal Zeros: STD_LOGIC_VECTOR(PCSize-3 DOWNTO 0);
+    SIGNal TempCarryOut: STD_LOGIC;
 BEGIN
-    
+    fAdder: entity work.nbitAdder generic map(PCSize) port map(PCcurrent, tempA, '0', tempF, TempCarryOut);
+Zeros <= (others => '0');
+tempA <= Zeros&ControlPCoperation;  --( if 00  we will add 0 on (pc stall)  ) | (if 01 we will add 1 (swap) ) | (if 10 we will add 2 (ordinary increment) )
+PCReg <= PCcurrent;
+process (clk)
+    begin
+        if (reset ='1') then 
+            PCcurrent<=(others =>'0');         ----  malhash lazma 5ales -----
+        elsif (clk'EVENT AND clk='1') then 
+                if (MemPcEnable ='1') then 
+                    PCcurrent<=MemPc;
+                elsif (ExPcEnable ='1') then
+                    PCcurrent<=ExPC;
+                else 
+                    PCcurrent <=tempF;
+                end if;
+        end if;
+    end process;
 
 END ARCHITECTURE;
