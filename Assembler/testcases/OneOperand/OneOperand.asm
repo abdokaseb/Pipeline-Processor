96a0d22e09f8db868439740b9bfc6b66ee99e76d
RamySaied1 <ramy.m.saied@gmail.com>
2019-05-03 16:47:07 +0200

    One operand do file

--- Assembler/testcases/OneOperand/OneOperand.asm
+++ Assembler/testcases/OneOperand/OneOperand.asm
@@ -11,14 +11,19 @@
 .ORG 10
 setC           #C --> 1
 NOP            #No change
+# nzc = 001
 ClRC           #C --> 0
-NOT R1         #R1 =FFFF , C--> no change, N --> 1, Z --> 0
+NOT R1         #R1 =FFFF , C--> no change, N --> 1, Z --> 0   nzc 
+# nzc = 100
 inc R1	       #R1 =0000 , C --> 1 , N --> 0 , Z --> 1
 in R1	       #R1= 5,add 5 on the in port,flags no change	
+# nzc = 011
 in R2          #R2= 10,add 10 on the in port, flags no change
 NOT R2	       #R2= FFEF, C--> no change, N -->1,Z-->0
+# nzc =101
 inc R1         #R1= 6, C --> 0, N -->0, Z-->0
 Dec R2         #R2= FFEE,C-->1 , N-->1, Z-->0
-out R1
-out R3
-out R2
+# nzc = 101
+out R1         # port =6
+out R3         # port = 0
+out R2         # port = ffee 
